03ea0db1e815750b426ebd335b443908a911252f
BlueRain94 <thanhvuvn94@gmail.com>
2024-09-18 15:39:41 +0700

    Edit Finding with multiple request/response

--- dojo/forms.py
+++ dojo/forms.py
@@ -1251,7 +1251,8 @@ class AddFindingForm(forms.ModelForm):
     impact = forms.CharField(widget=forms.Textarea, required=False)
     request = forms.CharField(widget=forms.Textarea, required=False)
     response = forms.CharField(widget=forms.Textarea, required=False)
-    endpoints = forms.ModelMultipleChoiceField(Location.objects.none(), required=False, label="Systems / Endpoints")
+    rr_count = 1
+    endpoints = forms.ModelMultipleChoiceField(Endpoint.objects.none(), required=False, label="Systems / Endpoints")
     endpoints_to_add = forms.CharField(max_length=5000, required=False, label="Endpoints to add",
                                help_text="The IP address, host name or full URL. You may enter one endpoint per line. "
                                          "Each must be valid.",
@@ -1548,9 +1549,10 @@ class FindingForm(forms.ModelForm):
             "invalid_choice": "Select valid choice: Critical,High,Medium,Low"})
     mitigation = forms.CharField(widget=forms.Textarea, required=False)
     impact = forms.CharField(widget=forms.Textarea, required=False)
-    request = forms.CharField(widget=forms.Textarea, required=False)
-    response = forms.CharField(widget=forms.Textarea, required=False)
-    endpoints = forms.ModelMultipleChoiceField(queryset=Location.objects.none(), required=False, label="Systems / Endpoints")
+    # request = forms.CharField(widget=forms.Textarea, required=False)
+    # response = forms.CharField(widget=forms.Textarea, required=False)
+    rr_count = 0
+    endpoints = forms.ModelMultipleChoiceField(queryset=Endpoint.objects.none(), required=False, label="Systems / Endpoints")
     endpoints_to_add = forms.CharField(max_length=5000, required=False, label="Endpoints to add",
                                help_text="The IP address, host name or full URL. You may enter one endpoint per line. "
                                          "Each must be valid.",
@@ -1609,8 +1611,24 @@ class FindingForm(forms.ModelForm):
 
         # self.fields['tags'].widget.choices = t
         if req_resp:
-            self.fields["request"].initial = req_resp[0]
-            self.fields["response"].initial = req_resp[1]
+            self.rr_count = len(req_resp)
+            for i in range(self.rr_count):
+                self.fields[f'request_{i}'] = forms.CharField(
+                widget=forms.Textarea,
+                required=False,
+                label=f'Request {i + 1}',
+                help_text=f'Request {i + 1}'
+                )
+
+                self.fields[f'response_{i}'] = forms.CharField(
+                    widget=forms.Textarea,
+                    required=False,
+                    label=f'Response {i + 1}',
+                    help_text=f'Response {i + 1}'
+                )
+
+                self.fields[f'request_{i}'].initial = req_resp[i].get_request()
+                self.fields[f'response_{i}'].initial = req_resp[i].get_response()
 
         if self.instance.duplicate:
             self.fields["duplicate"].help_text = "Original finding that is being duplicated here (readonly). Use view finding page to manage duplicate relationships. Unchecking duplicate here will reset this findings duplicate status, but will trigger deduplication logic."
